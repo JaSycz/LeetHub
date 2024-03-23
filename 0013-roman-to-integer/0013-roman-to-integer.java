4a085db22efd2f53fb431b5b5e38f32f5161e113
JaSycz <130904170+JaSycz@users.noreply.github.com>
2024-03-23 23:40:55 +0100

    Time: 2 ms (100.00%), Space: 44.5 MB (80.52%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.java
+++ 0013-roman-to-integer/0013-roman-to-integer.java
@@ -1,28 +1,43 @@
-import java.util.Hashtable;
-
 class Solution {
-    public int romanToInt(String s) {
-        Hashtable<Character,Integer> romanToDecimal = new Hashtable<>(); 
-        romanToDecimal.put('I',1);
-        romanToDecimal.put('V',5);
-        romanToDecimal.put('X',10);
-        romanToDecimal.put('L',50);
-        romanToDecimal.put('C',100);
-        romanToDecimal.put('D',500);
-        romanToDecimal.put('M',1000);
-        int decimal=0;
-        
-        for(int i=0; i<s.length();i++){
-            
-            if( i < s.length() -1 && romanToDecimal.get((s.charAt(i))) < romanToDecimal.get((s.charAt(i+1)))){
-                
-                decimal -= romanToDecimal.get((s.charAt(i)));
-            } else {
-                decimal += romanToDecimal.get((s.charAt(i)));
-            }
-
-       
+    private int transalator(char c)
+    {
+        switch(c)
+        {
+            case 'I':
+                return 1;
+            case 'V':
+                return 5;
+            case 'X':
+                return 10;
+            case 'L':
+                return 50;
+            case 'C':
+                return 100;
+            case 'D':
+                return 500;
+            case 'M':
+                return 1000;
+            default :
+                return 0;
         }
-         return decimal;        
+    }
+    public int romanToInt(String s) {
+        int result=0;
+        int curr=transalator(s.charAt(0));
+        for (int i=1;i<s.length();i++)
+        {
+            int next=transalator(s.charAt(i));
+            if(curr<next)
+            {
+                result-=curr;
+            }
+            else
+            {
+                result+=curr;
+            }
+            curr=next;
+        }
+        result+=curr;
+        return result;
     }
 }

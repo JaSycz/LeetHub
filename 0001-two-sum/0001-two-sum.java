995add7b3023b976ea3db66492f57a42beb8a564
JaSycz <130904170+JaSycz@users.noreply.github.com>
2024-03-29 16:21:28 +0100

    Time: 2 ms (97.85%), Space: 45 MB (28.61%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,16 +1,18 @@
-import java.util.HashMap;
 class Solution {
     public int[] twoSum(int[] nums, int target) {
         HashMap<Integer, Integer > table = new HashMap<>();
-        for (int i=0;i<nums.length;i++){
-            table.put(nums[i],i);
-        }
-        for (int i=0;i<nums.length;i++){
-            int complement = target - nums[i];        
-            if(table.containsKey(complement) && table.get(complement) != i ){
-                return new int[]{i,table.get(complement)};
+        int[] twoIndices = new int[2];
+        for(int i=0;i<nums.length; i++){
+            if(table.containsKey(nums[i])){
+                twoIndices[0] = i;
+                twoIndices[1] = table.get(nums[i]);
+                break;
             }
-        }     
-        return null;        
+            else{
+                table.put(target-nums[i],i);
+            }
+        }
+        return twoIndices;
+        
     }
 }

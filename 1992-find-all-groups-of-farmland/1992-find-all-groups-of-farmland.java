e6baa9317f1ad5586d83f52559d2519fe437f1d9
JaSycz <130904170+JaSycz@users.noreply.github.com>
2024-04-20 11:58:03 +0200

    Time: 14 ms (55.89%), Space: 71.9 MB (15.21%) - LeetHub

--- 1992-find-all-groups-of-farmland/1992-find-all-groups-of-farmland.java
+++ 1992-find-all-groups-of-farmland/1992-find-all-groups-of-farmland.java
@@ -1,37 +1,36 @@
 class Solution {
     public int[][] findFarmland(int[][] land) {
-        int[][] groups = new int[land.length*land[0].length][4];
-        int group = 0;
+        List<int[]> groups = new ArrayList<>();
+        int[] group = new int[4];
         for(int i = 0; i < land.length; i++){
             for(int j = 0; j< land[i].length; j++){
                 if(land[i][j] == 1){
-                    groupSearch(land,groups,i,j,group);
-                    group++;
+                    groupSearch(land,group,i,j);
+                    groups.add(group);
+                    group = new int[4];
                 }
             }
         }
-        int groupsFixed[][] = new int[group][4];
-        for(int k = 0; k < group; k++){
-            groupsFixed[k] = groups[k]; 
-        }
-        return groupsFixed;
+        
+        
+    return groups.stream().toArray(int[][] :: new);
     }
 
-    public void groupSearch(int[][]land, int[][] groups,int i, int j,int group){
+    public void groupSearch(int[][]land, int[] group,int i, int j){
         if(i >= land.length || i < 0 || j >= land[i].length || j < 0 || land[i][j] != 1 ) return; 
         if ( (i - 1 < 0 || land[i-1][j] == 0) && (j - 1 < 0 || land[i][j-1] == 0)){ 
-            groups[group][0] = i;
-            groups[group][1] = j;
+            group[0] = i;
+            group[1] = j;
         }
         if((i + 1 >= land.length || land[i+1][j] == 0)  && (j + 1 >= land[0].length || land[i][j+1] == 0)){
-            groups[group][2] = i;
-            groups[group][3] = j;
+            group[2] = i;
+            group[3] = j;
         }
         land[i][j] = -1;
-        groupSearch(land,groups,i-1,j,group);
-        groupSearch(land,groups,i,j-1,group);
-        groupSearch(land,groups,i+1,j,group);
-        groupSearch(land,groups,i,j+1,group);
+        groupSearch(land,group,i-1,j);
+        groupSearch(land,group,i,j-1);
+        groupSearch(land,group,i+1,j);
+        groupSearch(land,group,i,j+1);
 
     }
 }

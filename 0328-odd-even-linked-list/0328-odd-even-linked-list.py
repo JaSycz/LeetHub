1fbf69f861f30997f2f40d4856648372a1f768ae
JaSycz <130904170+JaSycz@users.noreply.github.com>
2024-09-04 23:25:59 +0200

    Time: 49 ms (6.64%), Space: 18.7 MB (9%) - LeetHub

--- 0328-odd-even-linked-list/0328-odd-even-linked-list.py
+++ 0328-odd-even-linked-list/0328-odd-even-linked-list.py
@@ -5,24 +5,27 @@
 #         self.next = next
 class Solution:
     def oddEvenList(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        if head is None or head.next is None or head.next.next is None:
+        if head == None or head.next == None or head.next.next == None:
             return head
-        odd_nodes =  ListNode(val=head.val)
-        even_nodes = ListNode(val=head.next.val)
-        current = head.next.next
-        count = 3
-        odd_tail,even_tail = odd_nodes,even_nodes
-
-        while current is not None:
-            if count % 2  == 0:
-                 even_tail.next = ListNode(val=current.val)
-                 even_tail = even_tail.next
-            else:
-                odd_tail.next = ListNode(val=current.val)
-                odd_tail = odd_tail.next
-                 
-            current = current.next
+        tmp = head
+        end = head
+        count = 0
+        while end.next is not None:
+            end = end.next
             count+=1
-        odd_tail.next = even_nodes
-        
-        return odd_nodes
+
+        if (count % 2 == 0):
+            count = count//2
+        else:
+            count = count//2 +1
+        while(count>0):
+            end.next = tmp.next
+            tmp.next = tmp.next.next
+            end.next.next = None
+
+            end = end.next
+            tmp = tmp.next
+
+            count-= 1
+
+        return head

3a17cb49c2bfe5198b6ab0ef3f677e6cbbebb71c
JaSycz <130904170+JaSycz@users.noreply.github.com>
2024-10-27 00:13:16 +0200

    Time: 51 ms (6.9%), Space: 20.4 MB (13.61%) - LeetHub

--- 1161-maximum-level-sum-of-a-binary-tree/1161-maximum-level-sum-of-a-binary-tree.py
+++ 1161-maximum-level-sum-of-a-binary-tree/1161-maximum-level-sum-of-a-binary-tree.py
@@ -6,43 +6,27 @@
 #         self.right = right
 class Solution:
     def maxLevelSum(self, root: Optional[TreeNode]) -> int:
-        queue = {1: [root]}
-        level_map = {}
-        level = 1
-        sum = 0
+        queue =  [root]
+        max_sum,level,ans = float(-inf),0,0
+        
         while queue:
-            current_node = queue[level].pop(-1)
-            if current_node:
-                sum += current_node.val
-                if level+1 not in queue.keys():
-                    if current_node.left and current_node.right:
-                      queue[level+1]=[current_node.left,current_node.right]
-                    elif current_node.left:
-                        queue[level+1]=[current_node.left]
-                    elif current_node.right:
-                        queue[level+1]=[current_node.right]
-                else:
-                  if current_node.left:
-                    queue[level+1].append(current_node.left)
-                  if current_node.right:
-                    queue[level+1].append(current_node.right)
-            
-            
-            if  queue[level] == []:
-                del queue[level]
-                level_map[level]= sum
-                sum = 0
-                level+=1
-                
-        print(level_map)
-        max = float(-inf)
-        max_key = 0
-        for key in level_map.keys():
-            if level_map[key] > max:
-                max = level_map[key]
-                max_key = key
-                
+            level+=1
+            sum_at_level=0
 
-        return max_key
+            for _ in range(len(queue)):
+              node = queue.pop(0)
+
+              sum_at_level += node.val
+              if node.left:
+                queue.append(node.left)
+              if node.right:
+                queue.append(node.right)
+                
+            if sum_at_level > max_sum:
+                max_sum = sum_at_level
+                ans = level
+        
+        
+        return ans
 
 

a64f53d1d970cf241bb6ac7c4de2464ea34195f6
JaSycz <130904170+JaSycz@users.noreply.github.com>
2024-10-29 12:09:09 +0100

    Time: 200 ms (39.02%), Space: 53.3 MB (13.5%) - LeetHub

---
+++ 1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero/1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero.py
@@ -0,0 +1,26 @@
+class Solution:
+    def minReorder(self, n: int, connections: List[List[int]]) -> int:
+        visited = set()
+        visited.add(0)
+        adj = defaultdict(list)
+        self.count = 0
+        def dfs(city):
+            for road in city:
+                if road[0] not in visited:
+                    if road[1] == 0:
+                        self.count+=1
+                    visited.add(road[0])
+                    dfs(adj[road[0]])    
+                
+
+
+        for i in range(n-1):
+            adj[connections[i][0]].append([connections[i][1],0])
+            adj[connections[i][1]].append([connections[i][0],-1])
+        
+        
+        dfs(adj[0])
+
+
+        return self.count
+        
